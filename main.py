5a65f9daf8372f0a8624cdbcbef124e316f06c70
dsmarcot2018 <drew.marcotte@mymail.champlain.edu>
2022-12-15 02:19:31 -0500

    final edits

--- main.py
+++ main.py
@@ -18,30 +18,85 @@ data['type'].replace('Strong Hot', 1, inplace=True)
 data['type'].replace('Weak Cold', 2, inplace=True)
 data['type'].replace('Strong Cold', 3, inplace=True)
 
-# Separate the features and the target variable
-X = data.drop(['degrees_from_mean'], axis=1)
-y = data['degrees_from_mean']
-
-# Create correlation matrix
 corr_matrix = data.corr()
 
-# Create a random forest regressor and fit it to the data
-regressor = RandomForestRegressor(verbose=3)
-regressor.fit(X, y)
+# X_default = data.drop(['degrees_from_mean'], axis=1) # -0.00162
+# X_no_id = data.drop(['id', 'degrees_from_mean'], axis=1) # -0.00143
+# X_no_date = data.drop(['date_str', 'degrees_from_mean'], axis=1) # -0.00153
+# X_no_long = data.drop(['longitude', 'degrees_from_mean'], axis=1) # -0.00234
+# X_no_lat = data.drop(['latitude', 'degrees_from_mean'], axis=1) # -0.00277
+# X_no_max = data.drop(['max_temp', 'degrees_from_mean'], axis=1) # 0.00074
+# X_no_min = data.drop(['min_temp', 'degrees_from_mean'], axis=1) # 0.00031
+# X_no_type = data.drop(['type', 'degrees_from_mean'], axis=1) # -0.00287
+# X_no_id_date_max_min = data.drop(['id', 'date_str', 'max_temp', 'min_temp', 'degrees_from_mean'], axis=1) # 0.00032
+# X_no_id_date_max = data.drop(['id', 'date_str', 'max_temp', 'degrees_from_mean'], axis=1) # 0.00057
+# X_no_id_date_min = data.drop(['id', 'date_str', 'min_temp', 'degrees_from_mean'], axis=1) # -0.00029
+# X_no_id_max_min = data.drop(['id', 'max_temp', 'min_temp', 'degrees_from_mean'], axis=1) # 0.00017
+# X_no_date_max_min = data.drop(['date_str', 'max_temp', 'min_temp', 'degrees_from_mean'], axis=1) # -0.00005
+# X_no_id_date = data.drop(['id', 'date_str', 'degrees_from_mean'], axis=1) # -0.00168
+# X_no_id_max = data.drop(['id', 'max_temp', 'degrees_from_mean'], axis=1) # 0.00074
+X_no_id_min = data.drop(['id', 'min_temp', 'degrees_from_mean'], axis=1) # 0.00012
+# X_no_date_max = data.drop(['date_str', 'max_temp', 'degrees_from_mean'], axis=1) # -0.00017
+# X_no_date_min = data.drop(['date_str', 'min_temp', 'degrees_from_mean'], axis=1) # -0.00066
+# X_no_max_min = data.drop(['max_temp', 'min_temp', 'degrees_from_mean'], axis=1) # 0.00003
 
-y_pred = regressor.predict(X)
+def run(X):
+    y = data['degrees_from_mean']
+    
+    regressor = RandomForestRegressor(verbose=3, n_estimators=10, n_jobs=-1)
+    regressor.fit(X, y)
 
-y_compare = []
+    y_pred = regressor.predict(X)
 
-for pred, actual in zip(y_pred, y):
-    y_compare.append(pred - actual)
+    y_compare = []
 
-y_compare_mean = round((sum(y_compare) / len(y_compare)), 5)
-accuracy = accuracy_score(y, y_pred)
+    for pred, actual in zip(y_pred, y):
+        y_compare.append(pred - actual)
 
-print(y_pred)
-print(accuracy)
-print(y_compare_mean)
+    y_compare_mean = round((sum(y_compare) / len(y_compare)), 5)
+
+    print(y_compare_mean)
+    return y_compare_mean
+
+# X_no_min_accuracy = []
+# X_no_id_date_max_min_accuracy = []
+# X_no_id_date_min_accuracy = []
+# X_no_id_max_min_accuracy = []
+# X_no_date_max_min_accuracy = []
+X_no_id_min_accuracy = []
+# X_no_date_max_accuracy = []
+# X_no_max_min_accuracy = []
+
+for i in range(10):
+    # run(X_default)
+    # run(X_no_id)
+    # run(X_no_date)
+    # run(X_no_long)
+    # run(X_no_lat)
+    # run(X_no_max)
+    # X_no_min_accuracy.append(run(X_no_min))
+    # run(X_no_type)
+    # X_no_id_date_max_min_accuracy.append(run(X_no_id_date_max_min))
+    # run(X_no_id_date_max)
+    # X_no_id_date_min_accuracy.append(run(X_no_id_date_min))
+    # X_no_id_max_min_accuracy.append(run(X_no_id_max_min))
+    # X_no_date_max_min_accuracy.append(run(X_no_date_max_min))
+    # run(X_no_id_date)
+    # run(X_no_id_max)
+    X_no_id_min_accuracy.append(run(X_no_id_min))
+    # X_no_date_max_accuracy.append(run(X_no_date_max))
+    # run(X_no_date_min)
+    # X_no_max_min_accuracy.append(run(X_no_max_min))
+
+# print('X_no_min_accuracy: ', round((sum(X_no_min_accuracy) / len(X_no_min_accuracy)), 5))
+# print('X_no_id_date_max_min_accuracy: ', round((sum(X_no_id_date_max_min_accuracy) / len(X_no_id_date_max_min_accuracy)), 5))
+# print('X_no_id_date_min_accuracy: ', round((sum(X_no_id_date_min_accuracy) / len(X_no_id_date_min_accuracy)), 5))
+# print('X_no_id_max_min_accuracy: ', round((sum(X_no_id_max_min_accuracy) / len(X_no_id_max_min_accuracy)), 5))
+# print('X_no_date_max_min_accuracy: ', round((sum(X_no_date_max_min_accuracy) / len(X_no_date_max_min_accuracy)), 5))
+print('X_no_id_min_accuracy: ', round((sum(X_no_id_min_accuracy) / len(X_no_id_min_accuracy)), 5))
+# print('X_no_date_max_accuracy: ', round((sum(X_no_date_max_accuracy) / len(X_no_date_max_accuracy)), 5))
+# print('X_no_max_min_accuracy: ', round((sum(X_no_max_min_accuracy) / len(X_no_max_min_accuracy)), 5))
+
 print(corr_matrix)
 print(data.head())
 print(data.info())
